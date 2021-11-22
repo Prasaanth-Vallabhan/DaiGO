1320939250fc3898458f4e33ef15aa132028b447
Prasaanth-Vallabhan <prasaanthvallabhan@gmail.com>
2021-11-22 10:45:26 +0400

    newsFeed commit

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import Custombutton from './Components/custombutton';
 
